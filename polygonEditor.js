fe76ed9cfa2d2a1806d5287d3364e6183727c2d3
User <zemlak@list.ru>
2019-02-15 17:33:29 +0300

    change url

--- polygonEditor.js
+++ polygonEditor.js
@@ -1,5 +1,5 @@
 ymaps.ready(init);
-service_url = 'http://127.0.0.1:5000/calcpops'
+service_url = 'https://service-integr.jet.su/rst/calcpops'
 
 function init() {
     var myMap = new ymaps.Map("map", {
